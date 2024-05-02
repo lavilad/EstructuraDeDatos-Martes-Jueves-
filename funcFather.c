dcb43425e2bd99a9ace0d27dce594b465877f9d8
emilianogalaviz <143670243+emilianogalaviz@users.noreply.github.com>
2024-05-02 13:08:18 -0600

    Update funcFather.c

--- funcFather.c
+++ funcFather.c
@@ -1,28 +1,32 @@
-//Emiliano Galaviz
+//Emiliano Galaviz y Tenoch Moreno
 include <stdio.h>
 include <stdlib.h>
 
 //Funcion para saber el padre de un nodo del arbol
-Node* searchFather(Node* root, int key){
-  //verifica si el arbol tiene datos
-  if(root == NULL || root->key == key){
-    return NULL;
+void searchFather(Node* root, int key){
+    //verifica si el arbol tiene datos
+    if(root == NULL || root->key == key){
+      printf("No se encontró el nodo o es la raíz del árbol.\n");
+        return;
+    }
+    //verifica si tiene mas de un nodo
+    if(root->left && root->left->key == key){
+      printf("El padre de %d es %d.\n", key, root->key);
+      return;
+    }
+    //verifica si tiene mas de un nodo
+    if(root->right && root->right->key == key){
+      printf("El padre de %d es %d.\n", key, root->key);
+      return;
+    }
+    //usa recursividad para encontrar el nodo, si esta a la izquierda
+    if(key < root->key){
+      searchFather(root->left, key);
+      return;
+    }
+    //usa recursividad para encontrar el nodo, si esta a la derecha
+    if(key > root->key){
+      searchFather(root->right, key);
+      return;
+    } 
   }
-  //verifica si tiene mas de un nodo
-  if(root->left && root->left->key == key){
-    return root;
-  }
-  //verifica si tiene mas de un nodo
-  if(root->right && root->right->key == key){
-    return root;
-  }
-  //usa recursividad para encontrar el nodo, si esta a la izquierda
-  if(key < root->key){
-    return searchFather(root->left, key);
-  }
-  //usa recursividad para encontrar el nodo, si esta a la derecha
-  if(key > root->key){
-    return searchFather(root->right, key);
-  }
-  return NULL;
-}
